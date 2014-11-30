2c482ef1f2b1958254f6b5e83fcfe679fc979d0d
Jacob Honoré <mail@jacobhonore.dk>
2014-11-30 15:55:26 +0100

    Add addflight feature

--- src/java/TravelAgencyObjects/Itinery.java
+++ src/java/TravelAgencyObjects/Itinery.java
@@ -3,25 +3,48 @@
  * and open the template in the editor.
  */
 package TravelAgencyObjects;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import ws.dtu.Exception_Exception;
 /**
  *
  * @author jacobhonore
  */
 public class Itinery {
-    private int iteneryNum;
+    private int itineryNum;
     private String customerName;
-    private int cardNumber;
+    private String cardNumber;
     private int cardMonth;
     private int cardYear;
+    private List<String> flightList = new ArrayList<String>();
     public Itinery() {
         Random rand = new Random();
-        this.iteneryNum = rand.nextInt((10000 - 1) + 1) + 1;
+        this.itineryNum = rand.nextInt((10000 - 1) + 1) + 1;
     }
-    public void setCreditCardInfo(String customerName, int cardNumber, int cardMonth, int cardYear) {
+    public void setCreditCardInfo(String customerName, String cardNumber, int cardMonth, int cardYear) {
         this.customerName = customerName;
         this.cardNumber = cardNumber;
         this.cardMonth = cardMonth;
         this.cardYear = cardYear;
+    }
+    public boolean addFlight(String flightNumber) throws Exception_Exception {
+        ws.dtu.AirlineResourceService service = new ws.dtu.AirlineResourceService();
+        ws.dtu.AirlineResource port = service.getAirlineResourcePort();
+        dk.dtu.imm.fastmoney.types.CreditCardInfoType creditCard = new dk.dtu.imm.fastmoney.types.CreditCardInfoType();
+        creditCard.setName(this.customerName);
+        creditCard.setNumber(this.cardNumber);
+        dk.dtu.imm.fastmoney.types.ExpirationDateType expirationDate = new dk.dtu.imm.fastmoney.types.ExpirationDateType();
+        expirationDate.setMonth(this.cardMonth);
+        expirationDate.setYear(this.cardYear);
+        creditCard.setExpirationDate(expirationDate);
+        boolean result = port.bookFlight(flightNumber, creditCard);
+        if (result == true) {
+            flightList.add(flightNumber);
+        }
+        return result;
+    }
+    public boolean isCreditCardInfoSet() {
+        return (customerName != null && cardNumber != null && cardMonth > 0 && cardYear > 0);
     }
 }
--- src/java/ws/dtu/TravelResource.java
+++ src/java/ws/dtu/TravelResource.java
@@ -1,6 +1,8 @@
 package ws.dtu;
 
 import TravelAgencyObjects.Itinery;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
@@ -25,22 +27,32 @@ public class TravelResource {
         session.setAttribute("itinery", itinery);
         return "New itinery created";
     }
-    /*@Path("addFlight")
+    @Path("addFlight")
     @GET
-    public String addFlight(@Context HttpServletRequest req) { 
+    public String addFlight(@Context HttpServletRequest req, @QueryParam("flightnumber") String flightNumber) {
+        if (flightNumber == null || "".equals(flightNumber) || flightNumber.isEmpty())
+            return "Please set the flightnumber parameter.";
         HttpSession session = req.getSession(true);
         Itinery itinery;
         itinery = (Itinery) session.getAttribute("itinery");
         if (itinery == null) 
             return "No itinery. Please create an itinery.";
-        return itinery.addFlight("Hi", "1324", "154");
+        if (!itinery.isCreditCardInfoSet())
+            return "Credit card info havent been set, please call setCreditCardInfo.";
+        try {
+            itinery.addFlight(flightNumber);
+        } catch (Exception_Exception ex) {
+            return ex.getMessage();
+        }
         session.setAttribute("itinery", itinery);
-        return "New itinery created";
-    }*/
-    @Path("addCreditCardInfo")
+        return "Flight has been booked and added to list of flights.";
+    }
+    @Path("setCreditCardInfo")
     @GET
     public String setCreditCardInfo(@Context HttpServletRequest req, @QueryParam("name") String name,
-     @QueryParam("number") int number,  @QueryParam("month") int month,  @QueryParam("year") int year) {
+     @QueryParam("number") String number,  @QueryParam("month") int month,  @QueryParam("year") int year) {
+        if (!(name != null && number != null && month > 0 && year > 0))
+            return "All parameters havent been set.";
         HttpSession session = req.getSession(true);
         Itinery itinery;
         itinery = (Itinery) session.getAttribute("itinery");
