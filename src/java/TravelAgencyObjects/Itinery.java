e050d445366544dea993be4c1f516301c9336667
Jacob Honoré <mail@jacobhonore.dk>
2014-11-30 14:36:25 +0100

    Add various changes

--- src/java/TravelAgencyObjects/Itinery.java
+++ src/java/TravelAgencyObjects/Itinery.java
@@ -3,14 +3,25 @@
  * and open the template in the editor.
  */
 package TravelAgencyObjects;
-
+import java.util.Random;
 /**
  *
  * @author jacobhonore
  */
 public class Itinery {
-    public int iteneryNum;
-    public Itinery(int iteneryNum) {
-        this.iteneryNum = iteneryNum;
+    private int iteneryNum;
+    private String customerName;
+    private int cardNumber;
+    private int cardMonth;
+    private int cardYear;
+    public Itinery() {
+        Random rand = new Random();
+        this.iteneryNum = rand.nextInt((10000 - 1) + 1) + 1;
+    }
+    public void setCreditCardInfo(String customerName, int cardNumber, int cardMonth, int cardYear) {
+        this.customerName = customerName;
+        this.cardNumber = cardNumber;
+        this.cardMonth = cardMonth;
+        this.cardYear = cardYear;
     }
 }
--- src/java/ws/dtu/TravelResource.java
+++ src/java/ws/dtu/TravelResource.java
@@ -1,10 +1,12 @@
 package ws.dtu;
 
 import TravelAgencyObjects.Itinery;
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 
 /**
  *
@@ -12,13 +14,40 @@ import javax.ws.rs.Path;
  */
 @Path("travel")
 public class TravelResource {
-    List<Itinery> itineries = new ArrayList<Itinery>();
     @Path("createItinery")
     @GET
-    public int createItinery() { 
-        int itineryNum = itineries.size()+1;
-        itineries.add(new Itinery(itineryNum));
-        return itineryNum;
-    }  
-    
+    public String createItinery(@Context HttpServletRequest req) { 
+        HttpSession session = req.getSession(true);
+        Itinery itinery;
+        //itinery = (Itinery) session.getAttribute("itinery");
+        //if (itinery == null) 
+            itinery = new Itinery();
+        session.setAttribute("itinery", itinery);
+        return "New itinery created";
+    }
+    /*@Path("addFlight")
+    @GET
+    public String addFlight(@Context HttpServletRequest req) { 
+        HttpSession session = req.getSession(true);
+        Itinery itinery;
+        itinery = (Itinery) session.getAttribute("itinery");
+        if (itinery == null) 
+            return "No itinery. Please create an itinery.";
+        return itinery.addFlight("Hi", "1324", "154");
+        session.setAttribute("itinery", itinery);
+        return "New itinery created";
+    }*/
+    @Path("addCreditCardInfo")
+    @GET
+    public String setCreditCardInfo(@Context HttpServletRequest req, @QueryParam("name") String name,
+     @QueryParam("number") int number,  @QueryParam("month") int month,  @QueryParam("year") int year) {
+        HttpSession session = req.getSession(true);
+        Itinery itinery;
+        itinery = (Itinery) session.getAttribute("itinery");
+        if (itinery == null) 
+            return "No itinery. Please create an itinery.";
+        itinery.setCreditCardInfo(name, number, month, year);
+        session.setAttribute("itinery", itinery);
+        return "Creditcard information has been stored.";
+    }
 }
